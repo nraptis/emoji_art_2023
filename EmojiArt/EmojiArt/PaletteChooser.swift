4acc9048b99cc0cc2b6634959a6466e24de99ba7
Nick Raptis <nraptis@gmail.com>
2023-10-24 22:42:08 -0400

    lecture 11, 1 hour 1 minute

---
+++ EmojiArt/EmojiArt/PaletteChooser.swift
@@ -0,0 +1,18 @@
+//
+//  PaletteChooser.swift
+//  EmojiArt
+//
+//  Created by Nicky Taylor on 10/24/23.
+//
+
+import SwiftUI
+
+struct PaletteChooser: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PaletteChooser()
+}
---
+++ EmojiArt/EmojiArt/PaletteStore.swift
@@ -0,0 +1,8 @@
+//
+//  PaletteStore.swift
+//  EmojiArt
+//
+//  Created by Nicky Taylor on 10/24/23.
+//
+
+import Foundation
