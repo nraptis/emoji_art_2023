7d225756ca2b376972c35dfa07a50c4621bb081e
Nick Raptis <nraptis@gmail.com>
2023-10-26 16:24:43 -0400

    lecture 13, 30 minute mark

---
+++ EmojiArt/EmojiArt/PaletteEditor.swift
@@ -0,0 +1,18 @@
+//
+//  PaletteEditor.swift
+//  EmojiArt
+//
+//  Created by Nicky Taylor on 10/26/23.
+//
+
+import SwiftUI
+
+struct PaletteEditor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PaletteEditor()
+}
