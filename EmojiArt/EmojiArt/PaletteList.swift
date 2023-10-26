1cb19969ccb25c37506c6cffa93499fbdb271b46
Nick Raptis <nraptis@gmail.com>
2023-10-26 19:22:18 -0400

    navigation, giant emojis

---
+++ EmojiArt/EmojiArt/PaletteList.swift
@@ -0,0 +1,18 @@
+//
+//  PaletteList.swift
+//  EmojiArt
+//
+//  Created by Nicky Taylor on 10/26/23.
+//
+
+import SwiftUI
+
+struct PaletteList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PaletteList()
+}
