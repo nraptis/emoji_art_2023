30722a30505a4ffcacf484f709c8b6a2f0dc590b
Nick Raptis <nraptis@gmail.com>
2023-10-26 20:21:42 -0400

    editable palette list, palette manager, re-arranged

---
+++ EmojiArt/EmojiArt/PaletteManager.swift
@@ -0,0 +1,22 @@
+//
+//  PaletteManager.swift
+//  EmojiArt
+//
+//  Created by Nicky Taylor on 10/26/23.
+//
+
+import SwiftUI
+
+struct PaletteManager: View {
+    var body: some View {
+        NavigationSplitView {
+            
+        } detail: {
+            
+        }
+    }
+}
+
+#Preview {
+    PaletteManager()
+}
